46a25532acc374e72edbdf285ea2fdf80299b60a
Patricio Mariano <patomdp00@gmail.com>
2021-11-04 23:10:41 +0100

    prueba event emitter

--- src/app/components/add-edit-post/add-edit-post.component.ts
+++ src/app/components/add-edit-post/add-edit-post.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Post } from 'src/app/models/post';
 
 @Component({
   selector: 'app-add-edit-post',
   templateUrl: './add-edit-post.component.html',
-  styleUrls: ['./add-edit-post.component.css']
+  styleUrls: ['./add-edit-post.component.css'],
 })
 export class AddEditPostComponent implements OnInit {
+  @Output() model = new EventEmitter<Post>();
+
   addEditForm: FormGroup;
-   descriptionValue: string = '';
-   labelValue: string = '';
+  descriptionValue = '';
+  labelValue = '';
+  //  model: Post = { title: '', body: '' };
 
   constructor() {
     this.addEditForm = new FormGroup({
-      descriptionValue: new FormControl(''),
-      labelValue: new FormControl('')
-
+      descriptionValue: new FormControl(this.descriptionValue),
+      labelValue: new FormControl(this.labelValue),
     });
-   }
-
-  ngOnInit(): void {
   }
 
-  onSubmit(): void {
-    console.log('Description and label values: ' , this.addEditForm.value);
+  ngOnInit(): void {}
+
+  onSubmit(event: any): void {
+    event.preventDefault();
+    console.log('Event: ', event);
+    this.model.emit({ title: this.labelValue, body: this.descriptionValue });
+    console.log(this.model);
+    // TODO: pasar this.model con un .unshift() al lugar donde se almacena el array.
   }
-
-
 }
